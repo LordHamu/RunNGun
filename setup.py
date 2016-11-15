04fb3a22859afdaf572b1eb145910b1d33f37cae
Clayton Headley <lordhamu@gmail.com>
2016-11-14 16:00:07 -0800

    Working with ex_Freeze to create exe file.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+executables = [
+    Executable('gamemain.py', base=base)
+]
+
+build_options = {'build_exe': {'include-files':['character', 'enemies', 'levels', 'objects', 'sprites']}}
+
+results = setup(name='Run And Gun',
+      version='0.1',
+      description='Shooter Game Engine',
+      options = build_options,
+      executables=executables
+      )
+
+print('done:', results)
